97be059531fa43cad6e286ed8bafe2fc7e988441
mannan2009 <86875277+mannan2009@users.noreply.github.com>
2021-11-02 17:21:40 +0530

    Add files via upload

---
+++ Main.js
@@ -0,0 +1,8 @@
+var first_name = "Mannan";
+var last_name = "Kapoor";
+var full_name = first_name .concat(last_name);
+console.log.print (full_name);
+var a = "150";
+var b = "22525";
+var add = a + b;
+console.log(add);
